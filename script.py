dec330ff0ae1f77dd7f8c9aad1de38c0f69e7239
Elina010173 <elinaeke@mail.ru>
2023-10-30 16:40:40 +0300

    edited

--- script.py
+++ script.py
@@ -1,41 +1,53 @@
 import csv
 from datetime import datetime
 
-# Создайте словарь для хранения суммы температур и количества измерений для каждого дня
-daily_temperatures = {}
+def calculate_average_temperature(input_filename, output_filename):
+    # Создайте словарь для хранения суммы температур и количества измерений для каждого дня
+    daily_temperatures = {}
 
-# Открываем CSV файл для чтения
-with open('HM_data_2013-12-18 10_42_00.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+    # Открываем CSV файл для чтения
+    with open(input_filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-    for row in reader:
-        start_time_str = row['Start time']
-        value = float(row['Value'])
+        for row in reader:
+            start_time_str = row['Start time']
+            value = float(row['Value'])
 
-        # Преобразовываем строку с временем в объект datetime
-        current_datetime = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S')
+            # Преобразовываем строку с временем в объект datetime
+            current_datetime = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S')
 
-        # Извлекаем дату из объекта datetime
-        current_date = current_datetime.date()
+            # Извлекаем дату из объекта datetime
+            current_date = current_datetime.date()
 
-        # Проверяем, есть ли уже запись для этой даты в словаре
-        if current_date in daily_temperatures:
-            daily_temperatures[current_date]['sum'] += value
-            daily_temperatures[current_date]['count'] += 1
-        else:
-            daily_temperatures[current_date] = {'sum': value, 'count': 1}
+            # Проверяем, есть ли уже запись для этой даты в словаре
+            if current_date in daily_temperatures:
+                daily_temperatures[current_date]['sum'] += value
+                daily_temperatures[current_date]['count'] += 1
+            else:
+                daily_temperatures[current_date] = {'sum': value, 'count': 1}
 
-# Открываем новый CSV файл для записи средних температур за каждый день
-with open('average_daily_temperatures.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Date', 'Average Temperature']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Открываем новый CSV файл для записи средних температур за каждый день
+    with open(output_filename, 'w', newline='') as csvfile:
+        fieldnames = ['Date', 'Average Temperature']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    # Записываем заголовок
-    writer.writeheader()
+        # Записываем заголовок
+        writer.writeheader()
 
-    # Записываем средние температуры за каждый день в новый файл
-    for date, data in daily_temperatures.items():
-        average_temperature = data['sum'] / data['count']
-        writer.writerow({'Date': date, 'Average Temperature': average_temperature})
+        # Записываем средние температуры за каждый день в новый файл
+        for date, data in daily_temperatures.items():
+            average_temperature = data['sum'] / data['count']
+            writer.writerow({'Date': date, 'Average Temperature': average_temperature})
 
-print("Средние температуры за каждый день были записаны в новый CSV файл 'average_daily_temperatures.csv'.")
+    print(f"Средние температуры за каждый день были записаны в новый CSV файл '{output_filename}'.")
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Calculate average temperature from a CSV file.")
+    parser.add_argument("input_file", help="Input CSV file name")
+    parser.add_argument("output_file", help="Output CSV file name")
+
+    args = parser.parse_args()
+
+    calculate_average_temperature(args.input_file, args.output_file)
--- scriptforfile.py
+++ scriptforfile.py
@@ -1,46 +1,58 @@
 import csv
-from datetime import datetime, timedelta
+from datetime import datetime
 
-data_to_write = []  # Создаем список для данных, которые будут записаны
+def process_csv(input_filename, output_filename):
+    data_to_write = []  # Создаем список для данных, которые будут записаны
 
-# Открываем CSV файл для чтения
-with open('HM_data_2013-12-18 10_42_00.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    
-    prev_date = None  # Переменная для хранения предыдущей даты
-    
-    for row in reader:
-        start_time_str = row['Start time']
-        value = float(row['Value'])
-        
-        # Преобразовываем строку с временем в объект datetime
-        current_date = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S').date()
-        
-        # Проверяем, если дата такая же, как и предыдущая, пропускаем запись
-        if prev_date is not None and current_date == prev_date:
-            continue
-        
-        # Создаем запись для записи в CSV файл
-        record = {
-            'Год': current_date.year,
-            'Месяц': current_date.month,
-            'День': current_date.day,
-            'Температура': value
-        }
-        
-        data_to_write.append(record)
-        
-        # Устанавливаем текущую дату как предыдущую для следующей итерации
-        prev_date = current_date
+    # Открываем CSV файл для чтения
+    with open(input_filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-# Открываем новый CSV файл для записи с использованием кодировки UTF-8
-with open('output.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['Год', 'Месяц', 'День', 'Температура']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    
-    writer.writeheader()
-    
-    for record in data_to_write:
-        writer.writerow(record)
+        prev_date = None  # Переменная для хранения предыдущей даты
 
-print("Данные с одинаковыми датами были удалены, и оставшиеся данные записаны в новый CSV файл 'output.csv'.")
+        for row in reader:
+            start_time_str = row['Start time']
+            value = float(row['Value'])
+
+            # Преобразовываем строку с временем в объект datetime
+            current_date = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S').date()
+
+            # Проверяем, если дата такая же, как и предыдущая, пропускаем запись
+            if prev_date is not None and current_date == prev_date:
+                continue
+
+            # Создаем запись для записи в CSV файл
+            record = {
+                'Год': current_date.year,
+                'Месяц': current_date.month,
+                'День': current_date.day,
+                'Температура': value
+            }
+
+            data_to_write.append(record)
+
+            # Устанавливаем текущую дату как предыдущую для следующей итерации
+            prev_date = current_date
+
+    # Открываем новый CSV файл для записи с использованием кодировки UTF-8
+    with open(output_filename, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Год', 'Месяц', 'День', 'Температура']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for record in data_to_write:
+            writer.writerow(record)
+
+    print(f"Данные с одинаковыми датами были удалены, и оставшиеся данные записаны в новый CSV файл '{output_filename}'.")
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Process a CSV file and remove duplicate date entries.")
+    parser.add_argument("input_file", help="Input CSV file name")
+    parser.add_argument("output_file", help="Output CSV file name")
+
+    args = parser.parse_args()
+
+    process_csv(args.input_file, args.output_file)
